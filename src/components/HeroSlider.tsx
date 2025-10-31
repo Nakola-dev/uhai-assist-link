3eb4cd0eefd2e186d549392951a12aebda88c2d6
Eldergod-McCullen <133879503+Eldergod-McCullen@users.noreply.github.com>
2025-10-31 17:43:34 +0300

    Update secondaryCTA path to include user segment

--- src/components/HeroSlider.tsx
+++ src/components/HeroSlider.tsx
@@ -31,7 +31,7 @@ const slides: HeroSlide[] = [            /* INITIAL SLIDER */
     },
     secondaryCTA: {
       text: "Learn First Aid",                /* MAKE VISIBLE BEFORE HOVER EFFECT */
-      path: "/dashboard",                     /* TRY WORKING WITH THE LOG IN LOGIC*/
+      path: "/dashboard/user",                     /* TRY WORKING WITH THE LOG IN LOGIC*/
     },
     imageAlt: "First responder assisting patient",
   },
